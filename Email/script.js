190749b0ba8edaa63d2a0f6f0aef01fc715dc6f6
Francesca Salvini <franci98sp@gmail.com>
2021-03-22 16:48:31 +0100

    completato esercizio email

---
+++ Email/script.js
@@ -0,0 +1,22 @@
+//Chiedi all’utente la sua email
+//Controlla che sia nella lista di chi può accedere
+//Stampa un messaggio appropriato sull’esito del controllo.
+
+var emailUtente = prompt("Dimmi la tua email");
+
+var emailDaVerificare = ["prova1@hotmail.it",  "prova2@hotmail.it" , "prova3@hotmail.it" ,  "prova4@hotmail.it" , "prova5@hotmail.it" , "prova6@hotmail.it" , "prova7@hotmail.it"];
+
+var isPresente = false;
+
+for(var i = 0; i < emailDaVerificare.length; i++) {
+
+    if(emailDaVerificare[1] === emailUtente) {
+        isPresente = true; 
+    }
+}
+
+if(isPresente) {
+    alert ("email trovata");
+} else {
+    alert ("email non trovata");
+}
